ca5213eed837ba9f3a7cb1a558b5ddfbc5e21316
adriennemadarang <adriennemadarang@icloud.com>
2023-08-08 02:36:32 -0700

    modified child shapes

--- index.js
+++ index.js
@@ -1,24 +1,10 @@
 const inquirer = require("inquirer");
 
 const fs = require("fs");
+const fileName = "./examples/logo.svg";
 
-const { Circle, Triangle, Square } = require("./lib/shapes");
+const shapeChoice = require("./lib/shapeChoice");
 
-class Svg {
-    constructor() {
-        this.textElement = ''
-        this.shapeElement = ''
-    }
-    render() {
-        return `<svg version="1.1" xmlns="http://www.w3.org/2000/svg" width="300" height="200">${this.shapeElement}${this.textElement}</svg>`
-    }
-    setTextElement(text, color) {
-        this.textElement = `<text x="150" y="125" font-size="60" text-anchor="middle" fill="${color}">${text}</text>`
-    }
-    setShapeElement(shape) {
-        this.shapeElement = shape.render()
-    }
-}
 
 // Array of questions 
 const questions = [
@@ -46,64 +32,20 @@ const questions = [
     },
 ];
 
-// Function to write data to fs
-function writeToFile(fileName, data) {
-    console.log("writing [" + data + "] to file [" + fileName + "]")
-    fs.writeFile(fileName, data, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Congrats! Here is your generated logo!");
+
+function createLogo(reponse) {
+    const svg = shapeChoice(response);
+    fs.writeFile(fileName, svg, ()=> console.log("Congrats! Here is your generated logo!"));
+}
+function init() {
+    inquirer
+    .prompt(questions)
+    .then((response) => {
+        createLogo(response);
+    })
+    .catch(err => {
+        console.log(err)
     });
 }
 
-async function init() {
-    console.log("Starting init");
-    var svgString = "";
-    var svgFile = "logo.svg";
-
-    const answers = await inquirer.prompt(questions);
-
-    var userText = "";
-    if (answers.text.length > 0 && answers.text.length < 4) {
-        userText = answers.text;
-    }
-    else {
-        console.log("Please enter 1-3 characters.");
-        return;
-    }
-    console.log("Text: [" + userText + "]");
-    fontColor = answers["text-color"];
-    console.log("Font color: [" + fontColor + "]");
-    shapeColor = answers.shape;
-    console.log("Shape color: [" + shapeColor + "]");
-    shape = answers["shape"];
-    console.log("User chose shape = [" + shape + "]");
-
-    let shapeType;
-    if (shape === "Square") {
-        shapeType = new Square();
-        console.log("square selected");
-    }
-    else if (shape === "Circle") {
-        shapeType = new Circle();
-        console.log("Circle selected");
-    }
-    else if (shape === "Triangle") {
-    shapeType = new Triangle();
-    console.log("Triangle selected");
-    }
-    else {
-        console.log("invalid shape");
-    }
-shapeType.setColor(shapeColor);
-
-var svg = new Svg();
-svg.setTextElement(userText, fontColor);
-svg.setShapeElement(shapeType);
-svgString = svg.render();
-
-writeToFile(svgFile, svgString);
-}
-
-init()
+init();
